b945a80f95bd1af999d2bf824001c85d7ddf357b
Lê Văn Toàn <114896961+July-27@users.noreply.github.com>
2024-07-21 23:08:16 +0700

    add file

---
+++ src/java/dto/FeedbackDTO.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author My Computer
+ */
+public class FeedbackDTO {
+    private int id;
+    private int vote;
+    private String content;
+    private String customerEmail;
+    private String doctorEmail;
+    private String customerAvatar;
+    private Date createDate;
+
+    public FeedbackDTO() {
+    }
+
+    public FeedbackDTO(int id, int vote, String content, String customerEmail, String customerAvatar) {
+        this.id = id;
+        this.vote = vote;
+        this.content = content;
+        this.customerEmail = customerEmail;
+        this.customerAvatar = customerAvatar;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerAvatar() {
+        return customerAvatar;
+    }
+
+    public void setCustomerAvatar(String customerAvatar) {
+        this.customerAvatar = customerAvatar;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getDoctorEmail() {
+        return doctorEmail;
+    }
+
+    public void setDoctorEmail(String doctorEmail) {
+        this.doctorEmail = doctorEmail;
+    }
+    
+    
+    
+    
+}
